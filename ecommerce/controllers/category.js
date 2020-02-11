728faa178144fdaa3b577c1a60160898b7425a56
Renanie Tanola <enan.tanola@gmail.com>
2020-02-11 15:56:52 -0800

    categories and products

--- ecommerce/controllers/category.js
+++ ecommerce/controllers/category.js
@@ -2,8 +2,6 @@ const Category = require('../models/category');
 const Product = require('../models/product');
 const { errorHandler } = require('../helpers/dbErrorHandler');
 
-
-
 exports.categoryById = (req, res, next, id) => {
     Category.findById(id).exec((err, category) => {
         if (err || !category) {
@@ -14,33 +12,30 @@ exports.categoryById = (req, res, next, id) => {
         req.category = category;
         next();
     });
-}
-
-exports.read = (req, res) => {
-    return res.json(req.category)
-
-}
+};
 
 exports.create = (req, res) => {
-    const category = new Category(req.body)
+    const category = new Category(req.body);
     category.save((err, data) => {
         if (err) {
-            return resizeBy.status(400).json({
+            return res.status(400).json({
                 error: errorHandler(err)
-            })
+            });
         }
-        res.json({ data })
-    })
-}
+        res.json({ data });
+    });
+};
+
+exports.read = (req, res) => {
+    return res.json(req.category);
+};
 
 exports.update = (req, res) => {
     console.log('req.body', req.body);
     console.log('category update param', req.params.categoryId);
-    // get the category
+
     const category = req.category;
-    // get category's name
     category.name = req.body.name;
-    // save
     category.save((err, data) => {
         if (err) {
             return res.status(400).json({
@@ -52,7 +47,6 @@ exports.update = (req, res) => {
 };
 
 exports.remove = (req, res) => {
-    // get category
     const category = req.category;
     Product.find({ category }).exec((err, data) => {
         if (data.length >= 1) {
@@ -60,7 +54,6 @@ exports.remove = (req, res) => {
                 message: `Sorry. You cant delete ${category.name}. It has ${data.length} associated products.`
             });
         } else {
-            // remove the category
             category.remove((err, data) => {
                 if (err) {
                     return res.status(400).json({
@@ -68,7 +61,7 @@ exports.remove = (req, res) => {
                     });
                 }
                 res.json({
-                    message: 'Category has been deleted'
+                    message: 'Category deleted'
                 });
             });
         }
@@ -76,7 +69,6 @@ exports.remove = (req, res) => {
 };
 
 exports.list = (req, res) => {
-    // get all the categories
     Category.find().exec((err, data) => {
         if (err) {
             return res.status(400).json({
--- ecommerce/controllers/product.js
+++ ecommerce/controllers/product.js
@@ -5,67 +5,83 @@ const Product = require('../models/product');
 const { errorHandler } = require('../helpers/dbErrorHandler');
 
 exports.productById = (req, res, next, id) => {
-    Product.findById(id).exec((err, product) => {
-        if (err || !product) {
-            return res.status(400).json({
-                error: 'Product could not be found'
-            })
-        }
-        req.product = product
-        next()
-    })
-}
+    Product.findById(id)
+        .populate('category')
+        .exec((err, product) => {
+            if (err || !product) {
+                return res.status(400).json({
+                    error: 'Product not found'
+                });
+            }
+            req.product = product;
+            next();
+        });
+};
 
 exports.read = (req, res) => {
-    // not send the photo for performance reason
-    req.product.photo = undefined
-    return res.json(req.product)
-}
+    req.product.photo = undefined;
+    return res.json(req.product);
+};
 
 exports.create = (req, res) => {
-    let form = new formidable.IncomingForm()
-    form.keepExtensions = true
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
     form.parse(req, (err, fields, files) => {
         if (err) {
             return res.status(400).json({
                 error: 'Image could not be uploaded'
-            })
+            });
         }
         // check for all fields
-        const { name, description, price, category, quantity, shipping } = fields
+        const { name, description, price, category, quantity, shipping } = fields;
 
         if (!name || !description || !price || !category || !quantity || !shipping) {
             return res.status(400).json({
                 error: 'All fields are required'
-            })
+            });
         }
 
-        let product = new Product(fields)
+        let product = new Product(fields);
 
         // 1kb = 1000
         // 1mb = 1000000
+
         if (files.photo) {
+            // console.log("FILES PHOTO: ", files.photo);
             if (files.photo.size > 1000000) {
                 return res.status(400).json({
-                    error: 'Image size is too large'
-                })
+                    error: 'Image should be less than 1mb in size'
+                });
             }
+            product.photo.data = fs.readFileSync(files.photo.path);
+            product.photo.contentType = files.photo.type;
         }
-        product.photo.data = fs.readFileSync(files.photo.path)
-        product.photo.contentType = files.photo.type
 
         product.save((err, result) => {
             if (err) {
-                console.log('Product create error', err)
+                console.log('PRODUCT CREATE ERROR ', err);
                 return res.status(400).json({
                     error: errorHandler(err)
-                })
+                });
             }
-            res.json(result)
-        })
-    })
+            res.json(result);
+        });
+    });
+};
 
-}
+exports.remove = (req, res) => {
+    let product = req.product;
+    product.remove((err, deletedProduct) => {
+        if (err) {
+            return res.status(400).json({
+                error: errorHandler(err)
+            });
+        }
+        res.json({
+            message: 'Product deleted successfully'
+        });
+    });
+};
 
 exports.update = (req, res) => {
     let form = new formidable.IncomingForm();
@@ -78,7 +94,6 @@ exports.update = (req, res) => {
         }
 
         let product = req.product;
-        // replace the current info using lodash extend method with new fields
         product = _.extend(product, fields);
 
         // 1kb = 1000
@@ -106,22 +121,6 @@ exports.update = (req, res) => {
     });
 };
 
-
-
-exports.remove = (req, res) => {
-    let product = req.product;
-    product.remove((err, deletedProduct) => {
-        if (err) {
-            return res.status(400).json({
-                error: errorHandler(err)
-            });
-        }
-        res.json({
-            message: 'Product has been deleted successfully'
-        });
-    });
-};
-
 /**
  * sell / arrival
  * by sell = /products?sortBy=sold&order=desc&limit=4
@@ -134,11 +133,10 @@ exports.list = (req, res) => {
     let sortBy = req.query.sortBy ? req.query.sortBy : '_id';
     let limit = req.query.limit ? parseInt(req.query.limit) : 6;
 
-    // get all the product
     Product.find()
-        .select('-photo') // deselect photo
-        .populate('category') // category is a type of ObjectId, thus we can use populate
-        .sort([[sortBy, order]]) // array of array
+        .select('-photo')
+        .populate('category')
+        .sort([[sortBy, order]])
         .limit(limit)
         .exec((err, products) => {
             if (err) {
@@ -154,13 +152,10 @@ exports.list = (req, res) => {
  * it will find the products based on the req product category
  * other products that has the same category, will be returned
  */
+
 exports.listRelated = (req, res) => {
-    // if we're getting request from the query get it
-    // else set the limit to 6
     let limit = req.query.limit ? parseInt(req.query.limit) : 6;
-    // find the related products not including the product itself
-    // ne - not including
-    // find the category
+
     Product.find({ _id: { $ne: req.product }, category: req.product.category })
         .limit(limit)
         .populate('category', '_id name')
@@ -172,11 +167,9 @@ exports.listRelated = (req, res) => {
             }
             res.json(products);
         });
-}
-
+};
 
 exports.listCategories = (req, res) => {
-    // get the categories used in Product model
     Product.distinct('category', {}, (err, categories) => {
         if (err) {
             return res.status(400).json({
@@ -199,21 +192,18 @@ exports.listBySearch = (req, res) => {
     let order = req.body.order ? req.body.order : 'desc';
     let sortBy = req.body.sortBy ? req.body.sortBy : '_id';
     let limit = req.body.limit ? parseInt(req.body.limit) : 100;
-    let skip = parseInt(req.body.skip); // skip for load more component in frontend
-    let findArgs = {}; // object will contain categoryId and the price range
+    let skip = parseInt(req.body.skip);
+    let findArgs = {};
 
     // console.log(order, sortBy, limit, skip, req.body.filters);
     // console.log("findArgs", findArgs);
 
-    // grap the key out of the object
     for (let key in req.body.filters) {
-        // if there's no length this part wont run
         if (req.body.filters[key].length > 0) {
             if (key === 'price') {
                 // gte -  greater than price [0-10]
                 // lte - less than
                 findArgs[key] = {
-                    // get the first key
                     $gte: req.body.filters[key][0],
                     $lte: req.body.filters[key][1]
                 };
@@ -224,7 +214,7 @@ exports.listBySearch = (req, res) => {
     }
 
     Product.find(findArgs)
-        .select('-photo') // do not select the photo
+        .select('-photo')
         .populate('category')
         .sort([[sortBy, order]])
         .skip(skip)
@@ -242,13 +232,53 @@ exports.listBySearch = (req, res) => {
         });
 };
 
-// will work like a middleware
 exports.photo = (req, res, next) => {
-    // if product has photo
     if (req.product.photo.data) {
-        // send the png,jpg, etc
         res.set('Content-Type', req.product.photo.contentType);
         return res.send(req.product.photo.data);
     }
     next();
+};
+
+exports.listSearch = (req, res) => {
+    // create query object to hold search value and category value
+    const query = {};
+    // assign search value to query.name
+    if (req.query.search) {
+        query.name = { $regex: req.query.search, $options: 'i' };
+        // assigne category value to query.category
+        if (req.query.category && req.query.category != 'All') {
+            query.category = req.query.category;
+        }
+        // find the product based on query object with 2 properties
+        // search and category
+        Product.find(query, (err, products) => {
+            if (err) {
+                return res.status(400).json({
+                    error: errorHandler(err)
+                });
+            }
+            res.json(products);
+        }).select('-photo');
+    }
+};
+
+exports.decreaseQuantity = (req, res, next) => {
+    let bulkOps = req.body.order.products.map(item => {
+        return {
+            updateOne: {
+                filter: { _id: item._id },
+                update: { $inc: { quantity: -item.count, sold: +item.count } }
+            }
+        };
+    });
+
+    Product.bulkWrite(bulkOps, {}, (error, products) => {
+        if (error) {
+            return res.status(400).json({
+                error: 'Could not update product'
+            });
+        }
+        next();
+    });
 };
